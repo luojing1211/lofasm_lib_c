c105b48e4b96e0bf7bd9190015031ecc5a4130f3
luojing1211 <jingluo@Jings-MacBook-Air.local>
2015-02-15 19:16:22 -0600

    Dedispersion code header file

--- Dedispersion/lofasm_de_dispersion.h
+++ Dedispersion/lofasm_de_dispersion.h
@@ -16,12 +16,21 @@
 #ifndef LOFASM_DE_DISPERSION_H_
 #define LOFASM_DE_DISPERSION_H_
 
-#include<stido.h>
+#include<stdio.h>
+#include "lofasm_data_IO.h"
 
+#define MAX_NUM_FILE 5
 #define MAX_FILENAME_SIZE 256
 
 typedef struct{
-	char * paramFile[MAX_FILENAME_SIZE];
-
+	char paramFile[MAX_FILENAME_SIZE];
+	char *dataFileNames[MAX_NUM_FILE];
+	int numDataFile;
 
 }dedispersion_param;
+
+int dedsps_read_flags(dedispersion_param *dedspsPar, int argc, char *argv[]);
+
+int dedsps_read_para_file(LoFASMIO *IOpar);
+
+#endif
