2fa4ef49760f2b5e487ea7446ae0f74a3ddbe557
luojing1211 <luojing1211@gmail.com>
2015-07-18 10:39:03 -0500

    modified header file

--- Dedispersion/include/lofasm_dedisperse_fltbank.h
+++ Dedispersion/include/lofasm_dedisperse_fltbank.h
@@ -19,7 +19,7 @@
 
 
 #include<vector>
-
+using namespace std;
 
 /*Define the filtbank data class*/
 class fltbank  
@@ -27,15 +27,46 @@ class fltbank
         int numFreqBin;
         int numTimeBin;
     public:
-        std::vector<std::vector<float>> fltdata;
-        std::vector<double> freqAxis;
+        vector<vector<float>> fltdata;
+        vector<double> freqAxis;
         double freqStep;
 
-        std::vector<double> timeAxis;
+        vector<double> timeAxis;
         double timeStep;
         fltbank (int numfbin,int numtbin);
-        void set_freqAxis(double fstart, double fStep, int numfbin);
-        void set_timeAxis(double tStart, double tStep, int numTbin);
+        void set_freqAxis(double fStart, double fStep);
+        void set_timeAxis(double tStart, double tStep);
         void set_data();
 }
+
+fltbank::fltbank(int numfbin,int numtbin){
+    numFreqBin = numfbin;
+    numTimeBin = numtbin;
+    fltdata.resize(numFreqBin, vector<float> (numTimeBin,0.0));
+}
+
+void fltbank::sef_freqAxis(double fstart, double fStep){
+    if(numFreqBin <= 0){
+        cout<<"The number of frequency bin should be more than zero"<<endl;
+        exit(1);
+    }
+    freqAxis.resize(numFreqBin,0.0);
+    freqStep = fStep;
+    for(int i =0;i<numFreqBin;i++){
+        freqAxis[i] = fStart+i*freqStep;
+    }
+}
+
+void fltbank::sef_timeAxis(double tStart, double tStep){
+    if(numTimeBin <= 0){
+        cout<<"The number of time bin should be more than zero"<<endl;
+        exit(1);
+    }
+    timeAxis.resize(numTimeBin,0.0);
+    timeStep = tStep;
+    for(int i =0;i<numTimeBin;i++){
+        timeAxis[i] = tStart+i*timeStep;
+    }
+}
+
 
