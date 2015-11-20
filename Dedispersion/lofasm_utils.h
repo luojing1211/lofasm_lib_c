339e7b068985e28a6695ef5ccfb161768bfbdd15
luojing1211 <jingluo@Jings-MacBook-Air-4.local>
2015-11-20 14:49:54 -0600

    add lofasm util file

---
+++ Dedispersion/lofasm_utils.h
@@ -0,0 +1,9 @@
+#ifndef LOFASM_UTILS_H_
+#define LOFASM_UTILS_H_
+#include"lofasm_dedsps_class.h"
+using namespace std;
+
+fltbank* combine_fltbank_time(fltbank & firstData, fltbank & secondData, int overwrite);
+
+
+#endif
